51a47068c2ecf5ec15a2b0382350476d126de419
Dstzahra <zahradastangoo@gmail.com>
2026-07-05 05:15:33 -0700

    Add hello_world example

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 01
+Topic: Running hello_world.py
+
+Description:
+This is the first program in the book, used to verify 
+that Python and the text editor are configured correctly.
+"""
+
+# The line of code to run in the editor:
+print("Hello Python world!")
