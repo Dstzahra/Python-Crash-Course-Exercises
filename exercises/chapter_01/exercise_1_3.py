9bb14c7da307ca8e6b632fb963bbe819fe5cbc32
Dstzahra <zahradastangoo@gmail.com>
2026-07-16 12:09:14 -0700

    Add exercise 1-3

---
+++ exercises/chapter_01/exercise_1_3.py
@@ -0,0 +1,12 @@
+"""
+1-3. Infinite Skills:
+
+* Analysis of images captured from stars and the cosmos.
+(تحلیل تصاویر ثبت‌ شده از ستاره‌ها و کیهان)
+
+* Analysis of medical data from a person's comprehensive check-up.
+(تحلیل داده‌های پزشکی حاصل از چک‌آپ کاملِ یک فرد)
+
+* Developing educational games.
+(توسعه بازی‌های آموزشی)
+"""
