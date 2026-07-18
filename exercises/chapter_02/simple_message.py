25abfae926a4b2463207b5a0ccf1014e08856ef5
Dstzahra <zahradastangoo@gmail.com>
2026-07-18 08:09:07 -0700

    Add documentation and comments to simple_message.py

--- exercises/chapter_02/simple_message.py
+++ exercises/chapter_02/simple_message.py
@@ -1,2 +1,15 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 02
+Topic: Variables and Simple Data Types - Variables Are Labels
+
+Description:
+This program demonstrates how to assign a message string to a variable
+and print its value, following the concept that variables act as labels.
+"""
+
+# Assign a message string to a variable.
 message = "Hello, welcome to our resturant."
+
+# Print the value of the message variable.
 print(message)
