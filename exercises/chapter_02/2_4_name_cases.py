197bc0753c785714bfb6a2ab3b87e3fe8772410d
Dstzahra <zahradastangoo@gmail.com>
2026-07-20 07:15:00 -0700

    Add exercise 2-4 name cases

---
+++ exercises/chapter_02/2_4_name_cases.py
@@ -0,0 +1,19 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 02
+Topic: Exercise 2-4: Name Cases
+
+Description:
+This program stores a person's name in a variable, and then 
+prints that person's name in lowercase, uppercase, and title case.
+"""
+
+Name_Cases = "zAhRa dAsTanGoO"
+lowercase = Name_Cases.lower()
+uppercase = Name_Cases.upper()
+title_case = Name_Cases.title()
+
+print(Name_Cases)
+print(lowercase)
+print(uppercase)
+print(title_case)
