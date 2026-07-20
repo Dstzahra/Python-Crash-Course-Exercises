e12b49ddc9387cabe91817c8713d48a9cf2e2662
Dstzahra <zahradastangoo@gmail.com>
2026-07-20 08:10:50 -0700

    Add exercise 2-5 famous quote

---
+++ exercises/chapter_02/2_5_famous_quote.py
@@ -0,0 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 02
+Topic: Exercise 2-5: Famous Quote
+
+Description:
+This program prints a series of famous quotes from various historical figures
+and thinkers, including a famous Persian poet, Ferdowsi.
+"""
+print('Steve Jobs said: "The people who are crazy enough to think they can change the world are the ones who do."')
+print('Elon Musk said: "Adversity shaped me into who I am today. It made my pain tolerance very high."')
+print('Nikola Tesla said: "The reason for others failing is that they give up when faced with hardship; but I believe perseverance is the key."')
+print('Albert Einstein said: "Logic will get you from A to B. Imagination will take you everywhere."')
+print('Marie Curie said: "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less."')
+print('Leonardo da Vinci said: "Learning never exhausts the mind."')
+print('William Shakespeare said: "The longest night will have an end."')
+print('Ferdowsi said: "توانا بود هر که دانا بود / ز دانش دل پیر برنا بود"')
