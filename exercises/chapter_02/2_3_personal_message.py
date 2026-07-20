bd9e4c15410fce97bd8646c02d181468a55c6332
Dstzahra <zahradastangoo@gmail.com>
2026-07-20 06:38:58 -0700

    Add chapter 2 exercise 2-3 personal message

---
+++ exercises/chapter_02/2_3_personal_message.py
@@ -0,0 +1,12 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 02
+Topic: Personal Message
+
+Description:
+This program uses a variable to represent a person's name, 
+and prints a personal message to that person.
+"""
+
+personal_message = "Hi Zahra, you will succeed"
+print(personal_message)
