6f89719b26b21e2bf14755300a971e96b70209bc
Dstzahra <zahradastangoo@gmail.com>
2026-07-20 08:38:18 -0700

    Add exercise 2-6 famous quote 2

---
+++ exercises/chapter_02/2_6_famous_quote2.py
@@ -0,0 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 02
+Topic: Exercise 2-6: Famous Quote 2
+
+Description:
+This program stores a famous person's name in a variable called famous_person, 
+and composes a message using another variable called message, then prints them.
+"""
+
+famous_person = "Steve Jobs"
+message = '"The people who are crazy enough to think they can change the world are the ones who do."'
+print(famous_person)
+print(message)
+
+# or :
+#print(f"{famous_person} said: {message}")
