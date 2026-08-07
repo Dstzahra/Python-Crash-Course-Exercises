58f0b30c933b3f8e7d721f706cfa4d9b1b8f21a4
Dstzahra <zahradastangoo@gmail.com>
2026-08-07 13:33:03 -0700

    Use string methods on list elements

--- examples/chapter_03/3_2_accessing_elements_in_a_list.py
+++ examples/chapter_03/3_2_accessing_elements_in_a_list.py
@@ -14,3 +14,7 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 # Access the first element in the list.
 # Indexes in Python start from 0.
 print(bicycles[0])
+
+# Use the title() method to format the element.
+# It makes the first letter uppercase.
+print(bicycles[0].title())
