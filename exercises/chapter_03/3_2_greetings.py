c864ae12cf686fa85b80570811b184cecca8450b
Dstzahra <zahradastangoo@gmail.com>
2026-08-10 00:18:17 +0330

    Add exercise 3-2 greetings

---
+++ exercises/chapter_03/3_2_greetings.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Exercise: 3-2 Greetings
+
+Description:
+This exercise practices using individual values from a list
+to create personalized messages.
+"""
+
+names = ['mehraban', 'sogol', 'motahareh']
+
+# Greet the first person.
+print(f"Hi {names[0].title()}! How are you, my friend?")
+
+# Greet the second person.
+print(f"Hi {names[1].title()}! How are you, my friend?")
+
+# Greet the third person.
+print(f"Hi {names[2].title()}! How are you, my friend?")
