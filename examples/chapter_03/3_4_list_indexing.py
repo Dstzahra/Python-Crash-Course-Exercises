7409a24eb30bf12ff48a9bc03b2d3b1d392be1aa
Dstzahra <zahradastangoo@gmail.com>
2026-08-09 23:41:33 +0330

    Add list indexing example

---
+++ examples/chapter_03/3_4_list_indexing.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Index Positions Start at 0, Not 1
+
+Description:
+This example demonstrates how to access individual values
+from a list using positive and negative indexes.
+"""
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# Access the second item in the list.
+print(bicycles[1])
+
+# Access the fourth item in the list.
+print(bicycles[3])
+
+# Access the last item in the list.
+print(bicycles[-1])
