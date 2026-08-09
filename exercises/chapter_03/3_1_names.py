f8950a42e9a7fa12ca4b6a1c285f5cd2e096b0c2
Dstzahra <zahradastangoo@gmail.com>
2026-08-10 00:03:40 +0330

    Add exercise 3-1 names

---
+++ exercises/chapter_03/3_1_names.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Exercise: 3-1 Names
+
+Description:
+This exercise practices storing names in a list and
+printing each name by accessing the elements one at a time.
+"""
+
+names = ['mehraban', 'sogol', 'motahareh']
+
+# Print the first person's name.
+print(names[0].title())
+
+# Print the second person's name.
+print(names[1].title())
+
+# Print the third person's name.
+print(names[2].title())
