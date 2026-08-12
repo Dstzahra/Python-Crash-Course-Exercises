f32dde6c2564cac499f0aa430f798fd46085dbab
Dstzahra <zahradastangoo@gmail.com>
2026-08-12 23:52:32 +0330

    Add list append example

---
+++ examples/chapter_03/3_7_appending_elements.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Appending Elements to the End of a List
+
+Description:
+This example demonstrates how to add a new element to the end
+of a list using the append() method.
+"""
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+# Print the original list.
+print(motorcycles)
+
+# Add a new element to the end of the list.
+motorcycles.append('ducati')
+
+# Print the modified list.
+print(motorcycles)
