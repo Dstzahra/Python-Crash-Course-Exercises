9d0aff4af37fb6625e8d4d81367b16bab1b55434
Dstzahra <zahradastangoo@gmail.com>
2026-08-12 22:29:56 +0330

    Add exercise 3-3 your own list

---
+++ exercises/chapter_03/3_3_your_own_list.py
@@ -0,0 +1,23 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Exercise: 3-3 Your Own List
+
+Description:
+This exercise practices creating a list of favorite cars
+and using the list to print statements about each one.
+"""
+
+cars = ['lamborghini', 'ferrari', 'mercedes-benz', 'tesla']
+
+# Print a statement about the first car.
+print(f"I would like to own a {cars[0].title()} for traveling.")
+
+# Print a statement about the second car.
+print(f"I would like to drive a {cars[1].title()} around the city.")
+
+# Print a statement about the third car.
+print(f"I would like to take a {cars[2].title()} to formal and everyday places.")
+
+# Print a statement about the fourth car.
+print(f"I like {cars[3].title()} because of its creativity, the Tesla name, and Elon Musk.")
