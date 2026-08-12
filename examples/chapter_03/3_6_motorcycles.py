baa987e1361d6e5129f3eba3a11e0a3674e659ea
Dstzahra <zahradastangoo@gmail.com>
2026-08-12 23:46:39 +0330

    Add list element modification example

---
+++ examples/chapter_03/3_6_motorcycles.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Modifying Elements in a List
+
+Description:
+This example demonstrates how to modify an element in a list
+by assigning a new value to a specific index.
+"""
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+# Print the original list.
+print(motorcycles)
+
+# Change the first element of the list.
+motorcycles[0] = 'ducati'
+
+# Print the modified list.
+print(motorcycles)
