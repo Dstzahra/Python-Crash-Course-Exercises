bf0bdaa2b0203dfaad1f4763f60e73d367aa26ab
Dstzahra <zahradastangoo@gmail.com>
2026-08-13 23:10:24 +0330

    Add list item removal examples

---
+++ examples/chapter_03/3_10_remove_items.py
@@ -0,0 +1,23 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Removing Items from a List
+
+Description:
+This example demonstrates how to remove items from a list
+using the del statement and index positions.
+"""
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+# Remove the first motorcycle from the list using its index.
+del motorcycles[0]
+print(motorcycles)
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+# Remove the second motorcycle from the list using its index.
+del motorcycles[1]
+print(motorcycles)
