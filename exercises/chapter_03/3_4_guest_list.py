eae4703675d1f4accdeb1db1c8453f6730ea980c
Dstzahra <zahradastangoo@gmail.com>
2026-08-18 20:57:08 +0330

    Add guest list exercise

---
+++ exercises/chapter_03/3_4_guest_list.py
@@ -0,0 +1,6 @@
+guests = ['Dad' , 'Mom' , 'Sis']
+print(guests)
+
+print(f"{guests[0]}, I would like to invite you to dinner.")
+print(f"{guests[1]}, I would like to invite you to dinner.")
+print(f"{guests[2]}, I would like to invite you to dinner.")
