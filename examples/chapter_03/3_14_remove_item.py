99998278795fc2030a856f548317c2957974c032
Dstzahra <zahradastangoo@gmail.com>
2026-08-23 23:02:22 +0330

    Update remove item example

--- examples/chapter_03/3_14_remove_item.py
+++ examples/chapter_03/3_14_remove_item.py
@@ -1,5 +1,17 @@
-motorcycles = ["honda", "yamaha", "suzuki" , "ducati"]
-print(motorcycles)
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Removing an Item by Value
 
+Description:
+This example demonstrates how to remove an item from a list
+by its value using remove().
+"""
+
+motorcycles = ["honda", "yamaha", "suzuki", "ducati"]
+
+# Remove a motorcycle by its value.
 motorcycles.remove("ducati")
+
+# Print the updated list.
 print(motorcycles)
