70ad24e7eb09ef54779fcb65f9c6c081b7e1f8b9
Dstzahra <zahradastangoo@gmail.com>
2026-08-23 23:47:58 +0330

    Add reverse cars example

---
+++ examples/chapter_03/3_18_reverse_cars.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Printing a List in Reverse Order
+
+Description:
+This example demonstrates how to reverse the order of a list
+using the reverse() method.
+"""
+
+cars = ["bmw", "audi", "toyota", "subaru"]
+
+# Print the original list.
+print(cars)
+
+# Reverse the order of the list.
+cars.reverse()
+
+# Print the reversed list.
+print(cars)
