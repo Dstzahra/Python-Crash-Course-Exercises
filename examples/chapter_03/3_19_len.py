c4738822644ad65427593eb44e1fe09d53d4b6cb
Dstzahra <zahradastangoo@gmail.com>
2026-08-28 22:47:04 +0330

    Complete Exercise 3-10: Every Function

---
+++ examples/chapter_03/3_19_len.py
@@ -0,0 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 03
+Topic: Finding the Length of a List
+
+Description:
+This example demonstrates how to find the number of items
+in a list using the len() function.
+"""
+
+cars = ["bmw", "audi", "toyota", "subaru"]
+
+# Print the original list.
+print(cars)
+
+# Find and print the length of the list.
+print(len(cars))
