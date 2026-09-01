afbf2137e860adcfcff9c612d85f2a1433aa3933
Dstzahra <zahradastangoo@gmail.com>
2026-09-01 22:58:00 +0330

    Add message after for loop

--- examples/chapter_04/4_2_magicians.py
+++ examples/chapter_04/4_2_magicians.py
@@ -1,11 +1,12 @@
 """
 Source: Python Crash Course (3rd Edition)
 Chapter: 04
-Topic: Doing More Work Within a for Loop
+Topic: Doing Something After a for Loop
 
 Description:
 This example demonstrates how to perform an action with each
-item in a list using a for loop.
+item in a list using a for loop and how to run a statement
+after the loop has finished.
 """
 
 magicians = ['alice', 'david', 'carolina']
@@ -16,3 +17,6 @@ for magician in magicians:
     # Print a personalized message for each magician.
     print(f"{magician.title()}, that was a great trick!")
     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# Print a message after the for loop has finished.
+print("Thank you, everyone. That was a great magic show!")
