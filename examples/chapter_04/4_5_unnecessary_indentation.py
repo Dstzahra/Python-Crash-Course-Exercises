d53c8ef6b48e5c2e66383658a940c7bad58eb53b
Dstzahra <zahradastangoo@gmail.com>
2026-09-03 00:05:28 +0330

    Add unnecessary indentation example

---
+++ examples/chapter_04/4_5_unnecessary_indentation.py
@@ -0,0 +1,18 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Indenting Unnecessarily
+
+Description:
+This example demonstrates how unnecessary indentation causes
+an IndentationError.
+"""
+
+message = "Hello, Python world!"
+
+# Print the message without unnecessary indentation.
+print(message)
+
+# Incorrect indentation: the print() statement has an unexpected indent.
+# message = "Hello, Python world!"
+#     print(message)
