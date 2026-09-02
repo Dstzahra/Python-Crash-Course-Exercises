af8630c9f208099986d480c9d3f467029a98c7fe
Dstzahra <zahradastangoo@gmail.com>
2026-09-02 23:34:42 +0330

    Add indentation error example

---
+++ examples/chapter_04/4_3_indentation_error.py
@@ -0,0 +1,19 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Forgetting to Indent
+
+Description:
+This example demonstrates how forgetting to indent the line
+after a for statement causes an IndentationError.
+"""
+
+magicians = ['alice', 'david', 'carolina']
+
+# Correct indentation: the print() statement is part of the for loop.
+# for magician in magicians:
+#     print(magician)
+
+# Incorrect indentation: the print() statement should be indented.
+for magician in magicians:
+print(magician)
