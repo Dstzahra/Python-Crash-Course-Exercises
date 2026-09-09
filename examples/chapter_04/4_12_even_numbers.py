0038b9e962a0f8925fe940b13ec031a9cf14f510
Dstzahra <zahradastangoo@gmail.com>
2026-09-10 00:33:30 +0330

    Add even numbers example

---
+++ examples/chapter_04/4_12_even_numbers.py
@@ -0,0 +1,15 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Using range() to Skip Numbers
+
+Description:
+This example demonstrates how to use the range() function
+with a step size to generate a list of even numbers.
+"""
+
+# Create a list of even numbers from 2 through 10.
+even_numbers = list(range(2, 11, 2))
+
+# Print the list of even numbers.
+print(even_numbers)
