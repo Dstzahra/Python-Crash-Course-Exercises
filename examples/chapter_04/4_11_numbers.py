66c1fcc8291710ee953e48cf6b35797cad4c988b
Dstzahra <zahradastangoo@gmail.com>
2026-09-10 00:21:55 +0330

    Add numbers list example

---
+++ examples/chapter_04/4_11_numbers.py
@@ -0,0 +1,15 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Using range() to Make a List of Numbers
+
+Description:
+This example demonstrates how to use the range() function
+and the list() function to create a list of numbers.
+"""
+
+# Create a list of numbers from 1 through 5.
+numbers = list(range(1, 6))
+
+# Print the list of numbers.
+print(numbers)
