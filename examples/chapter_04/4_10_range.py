ba15b1ecc4a40021fedb8211e8b253eff54400c7
Dstzahra <zahradastangoo@gmail.com>
2026-09-10 00:08:56 +0330

    Add range one argument example

---
+++ examples/chapter_04/4_10_range.py
@@ -0,0 +1,13 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Using the range() Function with One Argument
+
+Description:
+This example demonstrates how to use the range() function
+with one argument to generate and print numbers from 0 through 5.
+"""
+
+# Generate numbers from 0 through 5.
+for value in range(6):
+    print(value)
