994a438c1db3a707459ebeb37ccbb08db2ea5271
Dstzahra <zahradastangoo@gmail.com>
2026-09-09 23:54:57 +0330

    Add first numbers example

---
+++ examples/chapter_04/4_9_first_numbers.py
@@ -0,0 +1,13 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Using the range() Function
+
+Description:
+This example demonstrates how to use the range() function
+to generate and print a series of numbers.
+"""
+
+# Generate numbers from 1 through 4.
+for value in range(1, 5):
+    print(value)
