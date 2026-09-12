324369524b23d2980701744685d6e46d9e60a7fa
Dstzahra <zahradastangoo@gmail.com>
2026-09-12 23:41:43 +0330

    Add counting to twenty exercise

---
+++ exercises/chapter_04/4_3_counting_to_twenty.py
@@ -0,0 +1,12 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Counting to Twenty
+
+Description:
+This exercise uses a for loop to print the numbers from 1 through 20.
+"""
+
+# Print the numbers from 1 through 20.
+for value in range(1, 21):
+    print(value)
