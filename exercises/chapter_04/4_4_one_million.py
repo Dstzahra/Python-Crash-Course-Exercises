02b94e954e3ea90eac963b08b712174088c838be
Dstzahra <zahradastangoo@gmail.com>
2026-09-12 23:55:09 +0330

    Add one million exercise

---
+++ exercises/chapter_04/4_4_one_million.py
@@ -0,0 +1,16 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: One Million
+
+Description:
+This exercise creates a list of numbers from 1 through 1,000,000
+and uses a for loop to print the numbers.
+"""
+
+# Create a list of numbers from 1 through 1,000,000.
+numbers = list(range(1, 1000001))
+
+# Print each number in the list.
+for number in numbers:
+    print(number)
