14d4e7b37bdc6dfbf991290c07769dedad280c93
Dstzahra <zahradastangoo@gmail.com>
2026-09-15 22:55:31 +0330

    Add list slicing example

---
+++ examples/chapter_04/4_17_players.py
@@ -0,0 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Slicing a List
+
+Description:
+This example demonstrates how to access specific parts of a list
+using slicing.
+"""
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+# Print the first three players.
+print(players[0:3])
+
+# Print the second, third, and fourth players.
+print(players[1:4])
