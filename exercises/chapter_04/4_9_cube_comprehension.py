b029147a322e08eb914c3ee0b51665cbf0a1c80f
Dstzahra <zahradastangoo@gmail.com>
2026-09-15 22:33:35 +0330

    Add cube comprehension exercise

---
+++ exercises/chapter_04/4_9_cube_comprehension.py
@@ -0,0 +1,12 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Cube Comprehension
+
+Description:
+This exercise uses a list comprehension to create a list
+of the first 10 cubes.
+"""
+
+# Create a list of the first 10 cubes using a list comprehension.
+print([num ** 3 for num in range(1, 11)])
