fe9252e4e9ee16e09787ae873286267ebf6aeac8
Dstzahra <zahradastangoo@gmail.com>
2026-09-16 19:55:31 +0330

    Add slices exercise

---
+++ exercises/chapter_04/4_10_slices.py
@@ -0,0 +1,20 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Slices
+
+Description:
+This exercise demonstrates how to use slices to print
+the first three, middle three, and last three items in a list.
+"""
+
+places = ['Persepolis', 'Tromsø', 'Chabahar', 'Qeshm Island', 'Switzerland']
+
+print("\nThe first three items in the list are:")
+print(places[0:3])
+
+print("\nThree items from the middle of the list are:")
+print(places[1:4])
+
+print("\nThe last three items in the list are:")
+print(places[2:5])
