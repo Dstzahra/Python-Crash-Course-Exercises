8cc2640d2747b86d5e2cb4abbf6c137c50f8cce5
Dstzahra <zahradastangoo@gmail.com>
2026-09-16 19:35:35 +0330

    Add comments to slicing from end example

--- examples/chapter_04/4_19_slicing_from_end.py
+++ examples/chapter_04/4_19_slicing_from_end.py
@@ -1,2 +1,14 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Slicing from the End
+
+Description:
+This example demonstrates how to use negative indexes
+to create a slice from the end of a list.
+"""
+
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+# Print the last three players in the list.
 print(players[-3:])
