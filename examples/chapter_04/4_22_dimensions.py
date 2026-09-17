64b6a27848d758589137caa1e9a8b99d69437525
Dstzahra <zahradastangoo@gmail.com>
2026-09-17 16:06:16 +0330

    Add tuple dimensions example

---
+++ examples/chapter_04/4_22_dimensions.py
@@ -0,0 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Defining a Tuple
+
+Description:
+This example demonstrates how to define a tuple and access
+its elements using indexes.
+"""
+
+dimensions = (200, 50)
+
+# Print the first element.
+print(dimensions[0])
+
+# Print the second element.
+print(dimensions[1])
