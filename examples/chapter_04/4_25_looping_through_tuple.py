0a0a62987568f6837284840e008ddcc34a354c23
Dstzahra <zahradastangoo@gmail.com>
2026-09-19 18:20:23 +0330

    Add looping through tuple example

---
+++ examples/chapter_04/4_25_looping_through_tuple.py
@@ -0,0 +1,15 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Looping Through a Tuple
+
+Description:
+This example demonstrates how to loop through all the values
+in a tuple using a for loop.
+"""
+
+dimensions = (200, 50)
+
+# Loop through each value in the tuple.
+for dimension in dimensions:
+    print(dimension)
