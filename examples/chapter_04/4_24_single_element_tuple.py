bdbe48288ce57e25b7052a8556447562b0ee0c54
Dstzahra <zahradastangoo@gmail.com>
2026-09-19 18:11:49 +0330

    Add comments to single element tuple example

--- examples/chapter_04/4_24_single_element_tuple.py
+++ examples/chapter_04/4_24_single_element_tuple.py
@@ -1,3 +1,17 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: Single-Element Tuples
+
+Description:
+This example demonstrates how to define a tuple with one element.
+The comma is required to make it a tuple.
+"""
+
+# Define a tuple with one element.
 my_t = (3,)
+
 print(my_t)
+
+# Check the type of the variable.
 print(type(my_t))
