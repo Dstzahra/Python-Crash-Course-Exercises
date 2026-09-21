2effcece0ef14d43d47b9740dc53fcc496a0a68b
Dstzahra <zahradastangoo@gmail.com>
2026-09-21 23:37:41 +0330

    Add PEP 8 exercise

---
+++ exercises/chapter_04/4_14_pep_8.py
@@ -0,0 +1,9 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 04
+Topic: PEP 8
+
+Description:
+This exercise involves reviewing the official PEP 8 style guide.
+No code is required for this exercise.
+"""
