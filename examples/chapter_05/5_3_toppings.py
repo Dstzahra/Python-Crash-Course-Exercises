ec0af384afe81100fe4f13d8843737ef83a038d5
Dstzahra <zahradastangoo@gmail.com>
2026-09-24 22:12:41 +0330

    Inequality example

---
+++ examples/chapter_05/5_3_toppings.py
@@ -0,0 +1,16 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 05
+Topic: Checking for Inequality
+
+Description:
+This file demonstrates how to use the inequality operator (!=)
+to check whether two values are not equal.
+"""
+
+# Store the requested pizza topping.
+requested_topping = 'mushroom'
+
+# Check whether the requested topping is not anchovies.
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
