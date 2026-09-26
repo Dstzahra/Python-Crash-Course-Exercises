24fadb0834cf87ab2734fe47f88bb476d9454356
Dstzahra <zahradastangoo@gmail.com>
2026-09-26 12:20:11 +0330

    Simple if statement

---
+++ examples/chapter_05/5_6_voting.py
@@ -0,0 +1,16 @@
+"""
+Source: Python Crash Course (3rd Edition)
+Chapter: 05
+Topic: Simple if Statements
+
+Description:
+This example demonstrates how to use a simple if statement
+to check whether a person is old enough to vote.
+"""
+
+# Store the person's age.
+age = 19
+
+# Check whether the person is at least 18 years old.
+if age >= 18:
+    print("You are old enough to vote!")
