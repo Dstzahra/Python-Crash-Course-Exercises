ba33f1cd9cb49e0965f1b105ec678b65eb4fe8b4
Dstzahra <zahradastangoo@gmail.com>
2026-09-26 13:13:08 +0330

    Update voting example

--- examples/chapter_05/5_6_voting.py
+++ examples/chapter_05/5_6_voting.py
@@ -1,17 +1,22 @@
 """
 Source: Python Crash Course (3rd Edition)
 Chapter: 05
-Topic: Simple if Statements
+Topic: if-else Statements
 
 Description:
-This example demonstrates how to use a simple if statement
-to check whether a person is old enough to vote.
+This example demonstrates how to use an if-else statement
+to perform different actions depending on whether a condition is True or False.
 """
 
 # Store the person's age.
-age = 19
+age = 17
 
 # Check whether the person is at least 18 years old.
 if age >= 18:
     print("You are old enough to vote!")
     print("Have you registered to vote yet?")
+
+# Run this block if the condition is False.
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
